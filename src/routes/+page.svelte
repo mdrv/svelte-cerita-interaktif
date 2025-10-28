3bdd46e5c81049bec7ff1f55611fe704e9244ba6
Umar Alfarouk <medrivia@gmail.com>
2025-10-28 14:36:42 +0700

    First commit (with a game)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+import cerita from './data.js'
+let now = $state(cerita[0])
+let { judul, deskripsi, pilihan } = $derived(now)
+
+/**
+ * @param {number} rute
+ */
+function menuju(rute) {
+	let next = cerita.filter((item) => item.rute === rute)?.[0]
+	if (!next) {
+		alert("Rute tidak ditemukan!")
+	} else {
+		now = next
+	}
+}
+</script>
+
+<button onclick={() => menuju(0)}>🏠</button>
+<h2>{judul}</h2>
+<p>{deskripsi}</p>
+{#each pilihan as { teks, rute }}
+	<button onclick={() => menuju(rute)}>{teks}</button>
+{/each}
