ba1790426e5b1a50a8d5bd014dab9f58abadc501
Umar Alfarouk <medrivia@gmail.com>
2025-10-29 14:23:58 +0700

    Add og + metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+import favicon from '$lib/assets/favicon.svg'
+import { MetaTags } from 'svelte-meta-tags'
+let { children } = $props()
 </script>
+
+<MetaTags
+	title="Bearcu Adventure"
+	titleTemplate="%s | Bearcu Adventure"
+	description="Petualangan seru bersama beruang lucu"
+	canonical="https://bearcu.svelte.app"
+	openGraph={{
+		url: 'https://bearcu.svelte.app/',
+		title: 'Bearcu Adventure',
+		description: 'Petualangan seru bersama beruang lucu',
+		images: [
+			{
+				url: 'https://bearcu.svelte.app/og.jpg',
+				width: 1280,
+				height: 720,
+				alt: 'Bearcu Adventure',
+			},
+		],
+		siteName: 'Bearcu Adventure',
+	}}
+	twitter={{
+		creator: '@medrivia',
+		site: '@medrivia',
+		cardType: 'summary_large_image',
+		title: 'Bearcu Adventure',
+		description: 'Petualangan seru bersama beruang lucu',
+		image: 'https://bearcu.svelte.app/og.jpg',
+		imageAlt: 'Bearcu Adventure',
+	}}
+/>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
